47c350ab9fe6def2abc387f8ea29269c571713f2
lucas <klucas5227@gmail.com>
2023-12-30 13:40:34 +0900

    Initial Commit

---
+++ CGrammar/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  CGrammar
+//
+//  Created by Lucas Kim on 2023/12/30.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}

17da63b5384c188d5fd759a4a5a539c4e4fd380b
lucas <klucas5227@gmail.com>
2023-12-30 15:38:22 +0900

    1. C언어의 개요

---
+++ CGrammar/1-1 C언어의 개요.h
@@ -0,0 +1,13 @@
+//
+//  1-1 C언어의 개요.h
+//  CGrammar
+//
+//  Created by Lucas Kim on 2023/12/30.
+//
+
+#ifndef __1_C_____________h
+#define __1_C_____________h
+
+#include <stdio.h>
+
+#endif /* __1_C_____________h */
---
+++ CGrammar/1. C언어의 개요.h
@@ -0,0 +1,13 @@
+//
+//  1. C언어의 개요.h
+//  CGrammar
+//
+//  Created by Lucas Kim on 2023/12/30.
+//
+
+#ifndef ___C_____________h
+#define ___C_____________h
+
+#include <stdio.h>
+
+#endif /* ___C_____________h */
